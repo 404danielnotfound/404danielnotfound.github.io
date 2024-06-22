06a614db2c9076f656e95aa7f6ef538bdfc9dd43
Daniel Peng <danielpeng0311@outlook.com>
2024-06-22 22:19:53 +0800

    s

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2280553738"
+    serviceWorkerVersion: "3892629157"
   }
 });
