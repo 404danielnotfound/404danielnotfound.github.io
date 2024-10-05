7382760b9a83f138389cd42746833221e3398e2d
Daniel Peng <danielpeng0311@outlook.com>
2024-10-05 08:30:29 +0800

    sdf

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -18,7 +18,7 @@ loading.textContent = "Loading Entrypoint...";
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2739297268",
+    serviceWorkerVersion: "3595057406",
   },
   onEntrypointLoaded: async function(engineInitializer) {
     loading.textContent = "Initializing engine...";
